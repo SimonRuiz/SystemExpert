32866a396a124d352b317aa648a2f23b79d4b274
SimonRuiz <sim_ruruiz@hotmail.fr>
2017-05-10 17:10:18 +0200

    Commentaire explication SystemExpert

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * Interface utilisateur : input (defini toutes les conditions que l'on recherche
+ * Moteur d'inférence : récupére l'input
+ * regarde dans la base de fait si les conditions recherché ont déjà était diagnostiqué.
+ * Si non parcours en forward chaining dans la base de régles si les conditions sont répertorié
+ * en cas de réussite, enregistre les conditions et la réponse dans la base de fait
+ * sinon renvoye une erreur NotFound
+ */
 
 ?>
 
